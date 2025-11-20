ba2545adc265ee7b1d2b8c848462ce0b381b9392
abraham-peter <Peter.St.Abraham@student.utcluj.ro>
2025-11-20 15:25:03 +0200

    autentificare_test

--- backend/server/src/routes/auth.py
+++ backend/server/src/routes/auth.py
@@ -1,10 +1,15 @@
-from fastapi import APIRouter,status
+from fastapi import APIRouter,status,Form
 from schemas.user import PostCreate
+from typing import Annotated
+from schemas.auth import FormData
+
 router=APIRouter()
 
 @router.post("/auth/register",status_code=status.HTTP_200_OK)
 
 @router.post("/auth/login",status_code=status.HTTP_201_CREATED)
+async def login(data:Annotated[FormData,Form()]):
+    return data
 
 @router.post("/auth/refresh",status_code=status.HTTP_200_OK)
 
