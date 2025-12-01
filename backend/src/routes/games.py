e4de1b3eec416913421bed7882e26372884d4a32
abraham-peter <Peter.St.Abraham@student.utcluj.ro>
2025-12-01 15:01:31 +0200

    remaking folder structure+ adding frontend start

--- backend/src/routes/games.py
+++ backend/src/routes/games.py
@@ -1,10 +1,14 @@
 from fastapi import APIRouter,status
+from fastapi.responses import JSONResponse
 from schemas.game import PostResponse
 router=APIRouter()
-
+cuvant="MERE"
 
 @router.post("/session/{session_id}/games",response_model=PostResponse,status_code=status.HTTP_201_CREATED)
-
+async def test():
+    for i,char in enumerate(cuvant):
+        if char==litera_user:
+            return True
 @router.get("/sessions/{session_id}/games/{game_id}/state",status_code=status.HTTP_200_OK)
 
 @router.post("/sessions/{session_id}/games/{game_id}/guess",status_code=status.HTTP_200_OK)
