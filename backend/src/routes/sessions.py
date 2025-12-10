eb954de123816dafd54c773128f63f6bb6a73e4a
abraham-peter <Peter.St.Abraham@student.utcluj.ro>
2025-12-10 12:26:02 +0200

    update 0.2

--- backend/src/routes/sessions.py
+++ backend/src/routes/sessions.py
@@ -1,8 +1,13 @@
 from fastapi import APIRouter,status
-
+from schemas.session import PostCreate,PostResponse
 router=APIRouter()
 
 @router.post("/sessions",status_code=status.HTTP_201_CREATED)
+async def creaza_sesiune_joc(date:PostCreate)->PostResponse:
+    return PostResponse(
+        session_id="s_789",
+        num_games=date.num_games
+    )
 
 @router.get("/session/{session_id}",status_code=status.HTTP_201_CREATED)
 
