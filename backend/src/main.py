55142bcc24c98446f5b76f75552ba21bd185813d
SuciuSepty <Suciu.Se.Septimiu@student.utcluj.ro>
2025-12-10 13:31:35 +0200

    Update1

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,5 @@
 import uvicorn
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": # Initializarea inceput pentru aplicatia cu app, in folderul app cu numele app (foarte inspirat Peter :))
     uvicorn.run("app.app:app",port=8000,reload=True)
