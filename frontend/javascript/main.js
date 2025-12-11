ffc252eccc226893bd236e7a53b5ce3946ec8e24
abraham-peter <Peter.St.Abraham@student.utcluj.ro>
2025-12-11 17:46:43 +0200

    update 0.4 frontend meniu functional

--- frontend/javascript/main.js
+++ frontend/javascript/main.js
@@ -3,12 +3,7 @@ const btn=document.getElementById("start_button");
 btn.addEventListener("click",() => {
     console.log("Clicked!");
 });
-fetch("http://127.0.0.1:8000/games")
-.then(response => response.json())
-.then(data =>{
 
-}
 
-)
 
 });
