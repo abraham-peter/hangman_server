92b7e0fc581bdc72ee1d26e63eba045fbc5203f8
abraham-peter <Peter.St.Abraham@student.utcluj.ro>
2025-12-17 22:16:45 +0200

    frontend update 0.5

--- frontend/javascript/main.js
+++ frontend/javascript/main.js
@@ -1,9 +1,13 @@
 document.addEventListener("DOMContentLoaded",()=>{
-const btn=document.getElementById("start_button");
-btn.addEventListener("click",() => {
-    console.log("Clicked!");
-});
+const character=document.getElementById("character");
+ window.easy=()=>{
+    character.src="/frontend/images/baloon_child.png"
+ };
+ window.medium=()=>{
+    character.src="/frontend/images/n_baloon_child.png"
+ };
+ window.hard=()=>{
+    character.src="/frontend/images/h_baloon_child.png"
+ };
 
-
-
-});
+ });
