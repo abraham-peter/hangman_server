8d0abbb46816ed51b4ff27969ecb439d57177a95
septy <septyishere@gmail.com>
2026-01-11 11:20:23 +0200

    Update  v.0.5.5

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
 from hangman_server.backend.src.models import UserDB
+import dotenv 
+load_env()
 
 SQLALCHEMY_DATABASE_URL="postgresql://postgres:9GL7q8eNEf9c@localhost:5432/fastapi_db"
 
@@ -9,8 +11,7 @@ engine=create_engine(SQLALCHEMY_DATABASE_URL)
 Session_Local=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
 Base=declarative_base()
-UserDB.Base.metadata.create_all(bind=engine)
-
+Base.metadata.create_all(bind=engine)
 def get_db():
     db=Session_Local()
     try:
