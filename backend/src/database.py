778321ff3c22724f0f1b70600ba8489619ef77d0
abraham-peter <Peter.St.Abraham@student.utcluj.ro>
2026-01-12 10:05:02 +0200

    hide_databaseurl

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
-from hangman_server.backend.src.models import UserDB
-import dotenv 
-load_env()
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-SQLALCHEMY_DATABASE_URL="postgresql://postgres:9GL7q8eNEf9c@localhost:5432/fastapi_db"
+
+SQLALCHEMY_DATABASE_URL=os.getenv("SQLALCHEMY_DATABASE_URL")
 
 engine=create_engine(SQLALCHEMY_DATABASE_URL)
 
