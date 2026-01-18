310836c309420069a400875c35ad730fba10c4c1
Abraham <your.email@example.com>
2026-01-18 13:27:37 +0200

    fix database pentru server

--- backend/src/database.py
+++ backend/src/database.py
@@ -6,6 +6,8 @@ load_dotenv()
 
 
 SQLALCHEMY_DATABASE_URL=os.getenv("SQLALCHEMY_DATABASE_URL")
+if SQLALCHEMY_DATABASE_URL and SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine=create_engine(SQLALCHEMY_DATABASE_URL)
 
