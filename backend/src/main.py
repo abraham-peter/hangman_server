04998c3fc14e790f55b2402616168ec14fc252da
Abraham <your.email@example.com>
2026-01-18 12:55:43 +0200

    pyton bug fix incompatible version

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,4 +2,4 @@ import uvicorn
 
 
 if __name__ == "__main__": # Initializarea inceput pentru aplicatia cu app, in folderul app cu numele app (foarte inspirat Peter :))
-    uvicorn.run("app.app:app",port=8000,reload=True,)
+    uvicorn.run("app.app:app",port=8000,reload=True)
